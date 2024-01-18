bcbc48d880d36d2026af7e7298d9699515e6dc29
softdeveloper23 <brannongarrett77@gmail.com>
2024-01-18 11:58:16 +0100

    Finished with the application

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Counter program
+
+const decreaseBtn = document.getElementById('decreaseBtn');
+const increaseBtn = document.getElementById('increaseBtn');
+const resetBtn = document.getElementById('resetBtn');
+const countLabel = document.getElementById('countLabel');
+let count = 0;
+
+// Increase count
+increaseBtn.onclick = () => {
+    count++;
+    countLabel.textContent = count;
+}
+// Decrease count
+decreaseBtn.onclick = () => {
+    count--;
+    countLabel.textContent = count;
+}
+// Reset count
+resetBtn.onclick = () => {
+    count = 0;
+    countLabel.textContent = count;
+}
